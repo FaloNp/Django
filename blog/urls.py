62f6db5814218e79241da7d4764edadb55de811b
I <falo855o@gmail.com>
2024-01-14 18:44:47 +0100

    ?

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import add_post
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('add/', add_post, name='add_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+from django.shortcuts import render, redirect
+from .forms import PostForm
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/add_post.html', {'form': form})
