8bacba4c36d2b2f5c8ba6d1a29838063a74738db
I <falo855o@gmail.com>
2024-01-23 15:23:09 +0100

    finish

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    sort_by = request.GET.get('sort_by', '-published_date')
+    posts = Post.objects.order_by(sort_by)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -23,15 +24,15 @@ def post_detail(request, pk):
         form = CommentForm()
     return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
 
-def add_comment_to_post(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
-            comment.author = request.user
+            comment.author = request.user  # Ustawienie autora na zalogowanego użytkownika
             comment.save()
             return redirect('post_detail', pk=post.pk)
     else:
